d9208f5b64d0ec5583b64ab0ef8a2e112e63b8cb
秋雨落 <i@rain.cx>
2020-11-29 10:24:00 +0800

    忘了为啥要提交了，总之看到本地有更改就先提交
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ src/main/java/cx/rain/mc/fabric/rainminer/RainMiner.java
@@ -0,0 +1,10 @@
+package cx.rain.mc.fabric.rainminer;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RainMiner implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
